9ec9cec472ae8b968a389fe10c10f3d4c1731cbd
東雲なの <87938880+4Darmygeometry@users.noreply.github.com>
2025-04-02 01:42:50 +0800

    更新指令集判断脚本

--- build/linux/ISACheck.sh
+++ build/linux/ISACheck.sh
@@ -7,36 +7,40 @@ arch=$(uname -m)
 case $arch in
     x86_64)
         # 检查是否支持SSE4.2
-        if grep -q 'sse4_2' /proc/cpuinfo; then
+        if grep -iq 'sse4_2' /proc/cpuinfo; then
             echo "该CPU及Linux发行版支持SSE4.2扩展指令集，可以正常运行Watt Toolkit"
         else
             echo "该CPU或Linux发行版不支持SSE4.2扩展指令集，可能不能正常运行Watt Toolkit！"
         fi
         ;;
-    aarch64)
+    aarch64 | arm64)
         # 检查是否支持NEON
-        if grep -q 'neon' /proc/cpuinfo; then
-            echo "该CPU及Linux发行版支持NEON扩展指令集，可以正常运行Watt Toolkit"
+        if grep -iq 'neon' /proc/cpuinfo || grep -iq 'asimd' /proc/cpuinfo; then
+            echo "该CPU及Linux发行版支持ARM SIMD扩展指令集（NEON/ASIMD），可以正常运行Watt Toolkit"
         else
-            echo "该CPU或Linux发行版不支持NEON扩展指令集，可能不能正常运行Watt Toolkit！"
+            echo "该CPU或Linux发行版不支持ARM SIMD扩展指令集（NEON/ASIMD），可能不能正常运行Watt Toolkit！"
         fi
         ;;
     loongarch64)
-        # 检查是否支持LSX和LASX
-        if grep -q 'lsx' /proc/cpuinfo && grep -q 'lasx' /proc/cpuinfo; then
-            echo "该CPU及Linux发行版支持LSX和LASX扩展指令集，可以正常运行Watt Toolkit"
+        # 检查是否支持LSX和LASX，Watt Toolkit龙芯版的运行同时需要LSX与LASX
+        if grep -iq 'lsx' /proc/cpuinfo && grep -iq 'lasx' /proc/cpuinfo; then
+            echo "该CPU及Linux发行版同时支持LSX和LASX扩展指令集，可以正常运行Watt Toolkit"
         else
-            echo "该CPU或Linux发行版不支持LSX和LASX扩展指令集，可能不能正常运行Watt Toolkit！"
+            echo "该CPU或Linux发行版不支持LSX或LASX扩展指令集其中一种，可能不能正常运行Watt Toolkit！"
         fi
         ;;
     riscv64)
         # 检查ISA字段中的'v'扩展或显式cpu-vector字段
-        if { grep -m1 '^isa\s*:' /proc/cpuinfo | grep -q 'v'; } || \
+        if { grep -m1 '^isa\s*:' /proc/cpuinfo | grep -iqE '\bv[0-9]*\b'; } || \
         { grep -q 'cpu-vector' /proc/cpuinfo; }; then
             echo "该CPU及Linux发行版支持RISC-V的RVV向量扩展，可以正常运行Watt Toolkit"
         else
             echo "该CPU或Linux发行版不支持RISC-V的RVV向量扩展，可能不能正常运行Watt Toolkit！"
         fi
+        ;;
+    i?86 | arm*)
+        # 32位不再受到支持
+        echo "Watt Toolkit不再支持32位，32位用户请自行在Github/Gitee下载旧版使用，谢谢。"
         ;;
     *)
         echo "未知CPU架构：$arch"
