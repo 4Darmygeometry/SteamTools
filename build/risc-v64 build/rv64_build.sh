926416ed1ab670fbc65065c23cf50e04f6fb5553
泉わこ <87938880+4Darmygeometry@users.noreply.github.com>
2026-01-30 18:40:23 +0800

    Update .NET publish targets to net10.0

--- build/risc-v64 build/rv64_build.sh
+++ build/risc-v64 build/rv64_build.sh
@@ -2,9 +2,9 @@
 # set -e
 dotnet workload restore src/BD.WTTS.Client.Avalonia.App/BD.WTTS.Client.Avalonia.App.csproj
 #发布主程序
-dotnet publish -c Release src/BD.WTTS.Client.Avalonia.App/BD.WTTS.Client.Avalonia.App.csproj -p:UseAppHost=false -p:PublishDir=realbuild/assemblies -p:PublishSingleFile=false -p:PublishReadyToRun=false -p:PublishDocumentationFile=false -p:PublishDocumentationFiles=false -p:PublishReferencesDocumentationFiles=false -f net9.0 -r linux-riscv64 -v q /property:WarningLevel=1 --sc false --force --nologo -o "realbuild/assemblies"
+dotnet publish -c Release src/BD.WTTS.Client.Avalonia.App/BD.WTTS.Client.Avalonia.App.csproj -p:UseAppHost=false -p:PublishDir=realbuild/assemblies -p:PublishSingleFile=false -p:PublishReadyToRun=false -p:PublishDocumentationFile=false -p:PublishDocumentationFiles=false -p:PublishReferencesDocumentationFiles=false -f net10.0 -r linux-riscv64 -v q /property:WarningLevel=1 --sc false --force --nologo -o "realbuild/assemblies"
 #发布插件
-dotnet publish -c Release src/BD.WTTS.Client.Plugins.Accelerator.ReverseProxy/BD.WTTS.Client.Plugins.Accelerator.ReverseProxy.csproj -p:UseAppHost=true -p:PublishDir=realbuild/modules/Accelerator -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:PublishDocumentationFile=false -p:PublishDocumentationFiles=false -p:PublishReferencesDocumentationFiles=false -f net9.0 -r linux-riscv64 -v q /property:WarningLevel=1 --sc false --force --nologo -o "realbuild/modules/Accelerator"
+dotnet publish -c Release src/BD.WTTS.Client.Plugins.Accelerator.ReverseProxy/BD.WTTS.Client.Plugins.Accelerator.ReverseProxy.csproj -p:UseAppHost=true -p:PublishDir=realbuild/modules/Accelerator -p:PublishSingleFile=true -p:PublishReadyToRun=false -p:PublishDocumentationFile=false -p:PublishDocumentationFiles=false -p:PublishReferencesDocumentationFiles=false -f net10.0 -r linux-riscv64 -v q /property:WarningLevel=1 --sc false --force --nologo -o "realbuild/modules/Accelerator"
 
 dotnet build -c Release src/BD.WTTS.Client.Plugins.GameAccount/BD.WTTS.Client.Plugins.GameAccount.csproj --nologo -v q --property:WarningLevel=1 --property:DebugType=pdbonly -o "realbuild/modules/GameAccount"
 dotnet build -c Release src/BD.WTTS.Client.Plugins.GameList/BD.WTTS.Client.Plugins.GameList.csproj --nologo -v q --property:WarningLevel=1 --property:DebugType=pdbonly -o "realbuild/modules/GameList"
@@ -37,8 +37,8 @@ cp -a $dotnetloc $(dirname $dotnetloc)/host $(dirname $dotnetloc)/shared $(dirna
 mkdir native
 cd native
 mkdir linux-riscv64
-cp -a $HOME/.nuget/packages/skiasharp.nativeassets.linux/3.119.0-preview.1.2/runtimes/linux-riscv64/native/libSkiaSharp.so linux-riscv64/
-cp -a $HOME/.nuget/packages/harfbuzzsharp.nativeassets.linux/8.3.1.1-preview.1.2/runtimes/linux-riscv64/native/libHarfBuzzSharp.so linux-riscv64/
+cp -a $HOME/.nuget/packages/skiasharp.nativeassets.linux/3.119.2-preview.2.3/runtimes/linux-riscv64/native/libSkiaSharp.so linux-riscv64/
+cp -a $HOME/.nuget/packages/harfbuzzsharp.nativeassets.linux/8.3.1.3-preview.2.3/runtimes/linux-riscv64/native/libHarfBuzzSharp.so linux-riscv64/
 cp -a $HOME/.nuget/packages/sqlitepclraw.lib.e_sqlite3/2.1.11/runtimes/linux-riscv64/native/libe_sqlite3.so linux-riscv64/
 
 cd ..
